055eaba691811d339832301606e7bdb456855a3d
Abhishek Mahawar <mahawar.abhi@gmail.com>
2013-10-17 12:45:27 +0530

    Initial commit for clojure hello world web app.

---
+++ HelloWorld/project.clj
@@ -0,0 +1,11 @@
+(defproject HelloWorld "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
+(defproject helloworld "0.0.1"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
+  :uberjar-name "helloworld-standalone.jar"
+  :min-lein-version "2.0.0")
